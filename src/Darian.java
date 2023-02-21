8b0cab64709231990fd2b6ba6c42e3fbf110419c
DarianTan <tandari@sheridancollege.ca>
2023-02-21 14:06:06 -0500

    New Class, Added Comments

---
+++ src/Darian.java
@@ -0,0 +1,12 @@
+import javax.swing.text.html.MinimalHTMLWriter;
+
+public class Darian {
+    public static void main(String[] args) {
+        //------------------------------------------
+        //This is the Class called Darian
+        //Software Design Class 1231_18399
+        //Midterm 1 Part C 
+        //There is no code in this program just yet. 
+        //------------------------------------------
+    }
+}
